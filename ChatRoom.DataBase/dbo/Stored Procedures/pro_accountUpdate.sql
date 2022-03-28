85757e9114e3a1b2a4a90ef4dc5563b7c173e914
Tingyu <tingyu@championgame.net>
2022-03-28 16:07:08 +0800

    [Changed]密碼變更拉出 pro_accountUpdate.sql 獨立為一隻SP pro_accountChangePassword.sql

--- ChatRoom.DataBase/dbo/Stored Procedures/pro_accountUpdate.sql
+++ ChatRoom.DataBase/dbo/Stored Procedures/pro_accountUpdate.sql
@@ -1,18 +1,13 @@
 ﻿CREATE PROCEDURE [dbo].[pro_accountUpdate]
 	@account VARCHAR(40),--傳入值
-	@password VARCHAR(40),
 	@nickName NVARCHAR(20),
 	@isLocked	TINYINT,
 	@isMuted TINYINT, 
 	@errorTimes TINYINT,
 	@LoginIdentifier NVARCHAR(32)
 AS
-
-	DECLARE @passwordMD5 VARCHAR(32) = SUBSTRING(sys.fn_sqlvarbasetostr(HASHBYTES('MD5',@password)), 3, 32)
-
 	UPDATE t_account WITH(ROWLOCK)
 	SET 
-	f_password = @passwordMD5, 
 	f_nickName = @nickName,
 	f_isLocked = @isLocked,
 	f_isMuted = @isMuted,
