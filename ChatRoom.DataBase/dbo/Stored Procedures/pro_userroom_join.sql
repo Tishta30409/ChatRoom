d6b92c16ee6abebe3a04b3c874a6a8b968e058de
Tingyu <tingyu@championgame.net>
2022-03-28 18:03:32 +0800

    [fixed]補上ROWLOCK [added]leave新增回傳值

--- ChatRoom.DataBase/dbo/Stored Procedures/pro_userroom_join.sql
+++ ChatRoom.DataBase/dbo/Stored Procedures/pro_userroom_join.sql
@@ -3,9 +3,9 @@
 	@RoomID INT
 AS
 	IF EXISTS(SELECT Top 1 1 FROM t_userroom WHERE f_account = @Account)
-		UPDATE t_userroom SET f_roomID = @RoomID WHERE f_account = @Account
+		UPDATE t_userroom WITH(ROWLOCK) SET f_roomID = @RoomID WHERE f_account = @Account
 	ELSE
-		INSERT INTO t_userroom (f_account, f_roomID) VALUES (@Account, @RoomID)
+		INSERT INTO t_userroom (f_account, f_roomID) OUTPUT inserted.* VALUES (@Account, @RoomID)
 RETURN 0
 GO
 GRANT EXECUTE
--- ChatRoom.DataBase/dbo/Stored Procedures/pro_userroom_leave.sql
+++ ChatRoom.DataBase/dbo/Stored Procedures/pro_userroom_leave.sql
@@ -1,7 +1,9 @@
 ﻿CREATE PROCEDURE [dbo].[pro_userroom_leave]
 	@Account NVARCHAR(20)
 AS
-	UPDATE t_userroom SET f_roomID = NULL WHERE f_account = @Account
+	UPDATE t_userroom WITH(ROWLOCK) SET f_roomID = NULL 
+	OUTPUT  inserted.* 
+	WHERE f_account = @Account
 RETURN 0
 GO
 GRANT EXECUTE
