f40fa4b8ad3ab2be3338b1fd9b72da18914e8d66
Tingyu <tingyu@championgame.net>
2022-03-28 18:07:42 +0800

    [added]新增修改密碼的SP

---
+++ ChatRoom.DataBase/dbo/Stored Procedures/pro_accountChangePassword.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[pro_accountChangePassword]
+	@account VARCHAR(40),
+	@password VARCHAR(40)
+AS
+
+	DECLARE @passwordMD5 VARCHAR(32) = SUBSTRING(sys.fn_sqlvarbasetostr(HASHBYTES('MD5',@password)), 3, 32)
+
+	UPDATE t_account WITH(ROWLOCK)
+	SET 
+	f_password = @passwordMD5
+	OUTPUT inserted.*
+	WHERE f_account = @account
+RETURN 0
+GO
+GRANT EXECUTE
+    ON OBJECT::[dbo].[pro_accountChangePassword] TO PUBLIC
+    AS [dbo];
